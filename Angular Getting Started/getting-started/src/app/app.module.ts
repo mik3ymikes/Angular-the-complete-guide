19a625e136615a5b81493fc350c1b484d294fd8e
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-26 22:36:48 -0500

    more angular

--- Angular Getting Started/getting-started/src/app/app.module.ts
+++ Angular Getting Started/getting-started/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
