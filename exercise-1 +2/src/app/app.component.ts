c57ae73925ba099efa3f6a99e802ecc5b0152273
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-28 14:46:56 -0500

    directives problem

--- exercise-1 +2/src/app/app.component.ts
+++ exercise-1 +2/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercise-1 and 2';
-  username=""
+  title = 'exercise-1 and 2 and 3';
+  // username=""
+  showSecret=false;
+  log=[]
+
+  onToggleDetails(){
+    this.showSecret=!this.showSecret
+    this.log.push(this.log.length +1)
+  }
 }
