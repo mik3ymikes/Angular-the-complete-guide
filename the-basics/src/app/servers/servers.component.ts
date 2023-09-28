21116142a218f1751e03f410dadd0eabb927d892
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-27 23:06:43 -0500

    Cant find mistakes

--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-servers',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  selector: 'app-servers',
+  // template: `<app-server></app-server>
+  // <app-server></app-server>`,
   templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 allowNewServer=false;
-serverCreationStatus="no server was created"
+serverCreationStatus="no server was created";
+serverName="tester"
+
 
 constructor(){
   setTimeout(()=>{
     this.allowNewServer=true
-  })
+  }, 2000);
 }
 
 ngOnInit() {
 
 }
 onCreateServer(){
-  this.serverCreationStatus="server was created"
+  this.serverCreationStatus="server was created";
 }
+}
+
+onUpdateServerName(event: any){
+  console.log(event)
+  this.serverName=(<HTMLInputElement>event.target).value
 }
