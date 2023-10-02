27f1dc26ec180fb5a95a801052ca4df7a0e3ff07
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-01 21:56:37 -0500

    did some practice with property binding on booleans

--- exercise-1 +2/src/app/app.component.ts
+++ exercise-1 +2/src/app/app.component.ts
@@ -9,10 +9,15 @@ export class AppComponent {
   title = 'exercise-1 and 2 and 3';
   // username=""
   showSecret=false;
+  check=true
+
   log=[]
 
   onToggleDetails(){
     this.showSecret=!this.showSecret
+    this.check=!this.check
+
+
     // this.log.push(this.log.length +1)
     this.log.push(new Date())
   }
