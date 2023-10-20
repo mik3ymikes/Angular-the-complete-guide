f12cb9cfed12a2e04de3cb7bde460674a3af2dfe
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-20 16:41:49 -0500

    completed creating custom validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,18 +9,37 @@ import {FormGroup } from '@angular/forms'
 export class AppComponent implements Oninit {
   genders = ['male', 'female'];
   signupForm=FormGroup
+  forbiddenUsernames=['chirs', 'anna']
 
 
   ngONInit(){
     this.signupForm=new FormGroup({
-      'username': new FomrControl(null, Validators.required),
-      'email': new FomrControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FomrControl(null, [Validators.required, this.forbiddenUsernames.bind(this)]),
+        'email': new FomrControl(null, [Validators.required, Validators.email]),
+      })
       'gender': new FomrControl('male'),
+      'hobbies': new FormArray([])
 
 
     })
     onsubmit(){
       console.log(this.signupForm)
     }
+
+    onAddHobby(){
+      const control= new FormControl(null, Validators.required)
+   (<FormArray>this.signupForm.get('hobbies')).push(control)
   }
+
+
+  forbiddenNames( control: FormControl): {
+    [s: string : Boolean]
+
+    if (this.forbiddenUsernames.indexOf(control.value !== -1){
+      return {'nameIsForbidden' :true}
+    }
+    return null
+  }
+
 }
