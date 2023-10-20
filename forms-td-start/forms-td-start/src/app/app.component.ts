6aaea1937ce86c20a490324e552ed1957e55b7ec
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-20 15:15:58 -0500

    completed Td setting and patching form values

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -8,9 +8,24 @@ import { NgForm} from '@angular/forms'
 })
 export class AppComponent {
   @ViewChild('f') signupForm:NgFrom
+  defaultQuestion='teacher'
+  anwser=''
+  genders=['male', 'female']
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:'',
+    //   },
+    //   secret:'pet',
+    //   questionAnwser:'',
+    //   gender: 'male'
+    // })
+    this.signupForm.form.patchValue({userData:{
+      username:suggestedName
+    }})
   }
 
 
