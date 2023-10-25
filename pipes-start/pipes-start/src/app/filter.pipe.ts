414e18051c32b9b71ec787602c2eadc43ad58460
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-25 10:04:17 -0500

    completed up to creating a example filter pipe

---
+++ pipes-start/pipes-start/src/app/filter.pipe.ts
@@ -0,0 +1,23 @@
+import {PipeTransform, Pipe} from '@angular/core'
+
+@Pipe ({
+  name:'shorten'
+})
+
+export class FilterPipe implements PipeTransform{
+  transform(value:any, filterString:string, propName:string): any{
+    if (value.lenght ===0  || filterString=== ''){
+      return value
+    }
+    const resultArray=[]
+    for (const item of value){
+      if (item[propName]===filterString){
+        resultArray.push(item)
+      }
+    }
+    return resultArray
+  }
+
+}
+
+
