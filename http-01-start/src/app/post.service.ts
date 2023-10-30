4fc3e1c4544e61b204dc1e2444ad67615a9e129e
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-29 21:58:51 -0500

    completed up to services and compoenents working together

---
+++ http-01-start/src/app/post.service.ts
@@ -0,0 +1,51 @@
+import { injectable } from  '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { Post } from './ppost.model';
+import { map } from 'rxjs/operators'
+
+
+@injectable{providedIn:'root'}
+
+
+constructor(private http: HttpClient){
+
+}
+
+export class PostService{
+
+
+
+
+
+
+  createAndStorePost(title:string, content:string){
+    cosnt postData= Post={title:title, content:content}
+    this.http
+    .post<{name:string}>(
+      'https://ng-complete-guide-aa868-default-rtdb.firebaseio.com/posts.json',
+      postData
+      ).subscribe(responseData =>{
+        console.log(responseData)
+      })
+  }
+  fetchPosts(){
+    this.http
+    .get<{[key:string]: Post}>('https://ng-complete-guide-aa868-default-rtdb.firebaseio.com/posts.json')
+    .pipe(
+      map((responseData => {
+      const postsArray: Post[] =[]
+      for (const key in responseData){
+        if (responseData.hasOwnProperty(key)) {
+
+          postsArray.push({ ...responseData[key], id: key})
+        }
+      }
+      return postsArray
+    })
+    )
+    .subscribe(posts =>{
+      // this.isFetching=false
+      // this.loadedPosts=posts
+    })
+  }
+}
