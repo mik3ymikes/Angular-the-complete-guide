43f128a537ea080a1875341402691a6888b50eb5
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-08 14:25:35 -0600

    completed using keyframes for animations

--- animations-start/animations-start/src/app/app.component.ts
+++ animations-start/animations-start/src/app/app.component.ts
@@ -67,6 +67,49 @@ import { Component, trigger, state, style } from '@angular/core';
       ]
       }),
 
+      trigger('list2', [
+        state('in', style({
+         opacity:1
+          transform:'translateX(0)'
+        })),
+
+        transition('void =>*',
+        animate(1000, keyframes([
+          style({
+            transform:'translateX(-100px)',
+            opacity:0,
+            offset: 0
+          }),
+          style({
+            transform:'translateX(-50px)',
+            opacity:0.5,
+            offset:0.3
+          }),
+          style({
+            transform:'translateX(-20px)',
+            opacity:1,
+            offset:0.8
+          }),
+          style({
+            transform:'translateX(-20px)',
+            opacity:1,
+            offset:1
+          })
+        ]))
+        }),
+        [animate(300))]
+
+
+
+      ]),
+      transition('* =>void',
+      [animate(300), style({
+        transform"'translateX(100px),
+        opacity:0
+      })
+      ]
+      }),
+
   ]
 })
 export class AppComponent {
