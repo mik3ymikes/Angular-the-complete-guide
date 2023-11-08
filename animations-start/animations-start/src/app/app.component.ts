b02dc2667ce4e53ce2fe978da1a7d4c90ce63598
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-08 14:13:29 -0600

    completed the void state

--- animations-start/animations-start/src/app/app.component.ts
+++ animations-start/animations-start/src/app/app.component.ts
@@ -41,9 +41,31 @@ import { Component, trigger, state, style } from '@angular/core';
           borderRadius: '5-px'
         })),
         animate(500)
-      ]
+      ],
 
-      // transition('highlighted =>normal', animate(300))
+      trigger('list1', [
+        state('in', style({
+         opacity:1
+          transform:'translateX(0)'
+        })),
+
+        transition('void =>*',
+        style({
+          opacity:0,
+          transform: 'translateX(-100px)'
+        }),
+        [animate(300))]
+
+
+
+      ]),
+      transition('* =>void',
+      [animate(300), style({
+        transform"'translateX(100px),
+        opacity:0
+      })
+      ]
+      }),
 
   ]
 })
